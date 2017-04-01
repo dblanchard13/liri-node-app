3aea5436df4b600eadcec067690c32048989f3d7
Mikhail Metrikin <VolkRiot@users.noreply.github.com>
2017-03-31 21:03:04 -0700

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-exports.twitterKeys = {
-  consumer_key: 'CEFLU5tEwaRrygGfORwwWWoV2',
-  consumer_secret: 'nRO6bWmKrSEcpJkTK0yUXXPRgEkxiQx9lcAkyyuowKNYANhZg4',
-  access_token_key: '848013160271405056-GyfIaaGcjTHYVeTqTcRNNvePHY4P8Km',
-  access_token_secret: 'uaYd1GhnvqLDXPtNmZlxjsiBBBs8ztEzYALARa9oORRHP'
-}
