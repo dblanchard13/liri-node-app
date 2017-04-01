c940f4b2b74c280b59e2233249686a1a710e70c7
Mikhail Metrikin <metrikin@gmail.com>
2017-03-31 21:00:49 -0700

    Keys added

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'CEFLU5tEwaRrygGfORwwWWoV2',
+  consumer_secret: 'nRO6bWmKrSEcpJkTK0yUXXPRgEkxiQx9lcAkyyuowKNYANhZg4',
+  access_token_key: '848013160271405056-GyfIaaGcjTHYVeTqTcRNNvePHY4P8Km',
+  access_token_secret: 'uaYd1GhnvqLDXPtNmZlxjsiBBBs8ztEzYALARa9oORRHP'
 }
