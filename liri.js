e9a089112462e18397fc748d71069f418c52697f
Mikhail Metrikin <metrikin@gmail.com>
2017-04-01 21:28:26 -0700

    Dependencies and objectification

--- liri.js
+++ liri.js
@@ -1,7 +1,35 @@
-var twitterKeys = require('./keys.js').twitterKeys; // Twitter keys
-var request = require('request');
-var twitter = require('twitter');
+function LiriInterface(twitterKeys){
+  this.twitKeys = twitterKeys;
+  this.initDependencies();
+}
 
-var commArgs = process.argv.slice(2);
+LiriInterface.prototype.initDependencies = function(){
+
+  var TwitterBuild = require('twitter');
+
+  this.Twitter = new TwitterBuild(this.twitterKeys);
+  this.Requester = require('request');
+  this.Inquire = require('inquirer');
+
+};
+
+LiriInterface.prototype.promptUserChoice = function () {
+
+  this.user = prompt("Hello new friend, what should I call you?", "Mysterious Stranger");
+
+  this.Inquire.prompt([
+    {
+      type: "list",
+      message: "Hi! " + this.user + " , please select from the following options.",
+      name: 'order',
+      choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+    }
+  ])
+
+};
 
 
+
+// End of object prototypes
+
+var Liri = new LiriInterface(require('./keys.js').twitterKeys);
