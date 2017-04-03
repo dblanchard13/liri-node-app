2bd67f040f50e281cf82c61547afbd7bbb6cb2d0
Mikhail Metrikin <metrikin@gmail.com>
2017-04-02 19:00:49 -0700

    Added twitter and hoisting

--- liri.js
+++ liri.js
@@ -1,32 +1,81 @@
 function LiriInterface(twitterKeys){
+
   this.twitKeys = twitterKeys;
+  this.commandArray = ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'];
   this.initDependencies();
+  this.promptUserChoice();
+
 }
 
 LiriInterface.prototype.initDependencies = function(){
 
   var TwitterBuild = require('twitter');
 
-  this.Twitter = new TwitterBuild(this.twitterKeys);
+  this.Twitter = new TwitterBuild(this.twitKeys);
   this.Requester = require('request');
   this.Inquire = require('inquirer');
+  this.fileIO = require('fs');
+  this.Spotify = require('spotify');
 
 };
 
+
 LiriInterface.prototype.promptUserChoice = function () {
 
-  this.user = prompt("Hello new friend, what should I call you?", "Mysterious Stranger");
+  var self = this;
+  //this.user = prompt("Hello new friend, what should I call you?", "Mysterious Stranger");
 
   this.Inquire.prompt([
     {
       type: "list",
       message: "Hi! " + this.user + " , please select from the following options.",
-      name: 'order',
-      choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+      name: 'command',
+      choices: this.commandArray
     }
-  ])
+  ]).then(function(userInputs) {
+
+    self.userInputs = userInputs;
+    console.log(self.userInputs.command);
+    self.delegate(self.userInputs.command);
+
+  });
 
 };
+
+LiriInterface.prototype.delegate = function (selection) {
+
+  this.currentCommand = selection;
+
+  switch (this.currentCommand){
+
+    case 'my-tweets':
+
+      this.getTweets();
+
+      break;
+
+    case 'spotify-this-song':
+
+      break;
+
+    case 'movie-this':
+
+  }
+
+};
+
+LiriInterface.prototype.getTweets = function () {
+
+  this.Twitter.get('statuses/user_timeline', 'mishkatronic', function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+      //console.log(response);
+    }
+  });
+
+};
+
+
 
 
 
